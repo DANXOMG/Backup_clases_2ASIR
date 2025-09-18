3778dea51e6faeabf9d92f099e151f8efa1fb8dd
DANXOMG <gonzalezdlpinodaniel@gmail.com>
2025-09-18 16:41:16 +0200

    datos actualizados

---
+++ implantacion_aplicaciones_web/php/php002_variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uso de variables</title>
+</head>
+<body>
+
+    <?php 
+    /* Declaracion de variables */
+    $entero = 4; // tipo entero
+    $numero = 6.9; //tipo flotante float
+    $cadena = "Esto es una cadena"; // tipo string
+    $cierto = TRUE; //tipo boolean
+    $falso = FALSE; // tipo boolean
+
+    // Mostrar los valores en pantalla
+    echo "<p> entero = $entero </p>";
+    echo "<p> numero = $numero <p>";
+    echo "<p> cadena = $cadena <p>";
+    echo "<p> cierto = $cierto <p>";
+        echo "<p> falso = $falso </p>";
+
+
+
+    ?>
+    
+</body>
+</html>
