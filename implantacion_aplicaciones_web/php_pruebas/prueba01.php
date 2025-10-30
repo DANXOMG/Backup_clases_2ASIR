a2cd787d295ba76201427811c01dd458e6bc3cd7
DANXOMG <gonzalezdlpinodaniel@gmail.com>
2025-10-30 12:31:06 +0000

    update

---
+++ implantacion_aplicaciones_web/php_pruebas/prueba01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba php</title>
+</head>
+<body>
+    
+    <h1>Esto es mi primera prueba en php</h1>
+
+    <?php
+    echo "¡Hola, Daniel!";
+    ?>
+
+
+
+
+</body>
+</html>
