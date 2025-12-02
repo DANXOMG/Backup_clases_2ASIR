e6a37cdf742ae63a2bf6fee0d5121ef243994313
DANXOMG <gonzalezdlpinodaniel@gmail.com>
2025-12-02 19:20:02 +0000

    actualizacion php

---
+++ implantacion_aplicaciones_web/php/php_objetos/objetos1.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h1> Datos Recibidos.... </h1>";
+echo " <p> el usuario recibido es: " . $_POST["usuario"] . "</p>";
+echo " <p> la clave recibida es: " . $_POST["clave"] . "</p>";
+
+
+
+?>
